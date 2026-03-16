a77337df411faa2f7d52e1c983b2b29a6cfa470e
shoebjoy <shoebmahmud.joy@gmail.com>
2026-03-16 12:44:42 +0600

    navlink changed

--- src/app/components/NavLink.tsx
+++ src/app/components/NavLink.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 
 interface NavLinkProps {
-  onClick: () => void;
+  href: string;
   title: string;
+  onClick?: () => void; // keep optional in case you want click handling
 }
 
-const NavLink: React.FC<NavLinkProps> = ({ onClick, title }) => {
+const NavLink: React.FC<NavLinkProps> = ({ href, title, onClick }) => {
+  const handleClick = () => {
+    if (onClick) onClick();
+    // Navigate to href
+    window.location.href = href;
+  };
+
   return (
-    <div className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white cursor-pointer" onClick={onClick}>
+    <div
+      className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white cursor-pointer"
+      onClick={handleClick}
+    >
       {title}
     </div>
   );
